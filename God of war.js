8dbe4a0645225ec284398b1496facf61bd649e80
PeterLSY <89462460+PeterLSY@users.noreply.github.com>
2022-04-21 21:48:07 -0400

    Add files via upload

---
+++ God of war.js
@@ -0,0 +1,8 @@
+function change(){
+	var select = document.getElementById('kenny');
+	select.style.color='white';
+	select.style.backgroundColor='black';
+	select.innerHTML='Sony';
+	select.style.textAlign='right';
+	select.style.fontSize='30px'
+}
---
+++ Js Assighment 3 - God of War.js
@@ -0,0 +1,28 @@
+function change0(){
+    var x = getElementById('gowIcon2').style
+    x.width: 80%;
+    x.marginTop= '150px' ;
+    x.marginBottom= '150px';
+    x.height: 80%;
+    x.position= 'fixed';
+    x.opacity: 1.0;
+    x.transistion = '2s ease';
+}
+
+function change1(){
+    x.var x = getElementById('ved').style
+    x.color: white;
+    x.font-size= '30px';
+    x.padding= '20px';
+    x.width= '200px';
+    x.margin-bottom= '450px';
+    x.margin-top= 'auto';
+    x.text-align= 'center';
+    x.border-style= 'none';
+    x.background-color= 'darkred';
+    x.box-shadow= '0 0 40px darkred';
+    x.text-decoration= 'none';
+    x.opacity= '1';
+    x.transition= '1.5s';
+    x.width= '300px';
+}
